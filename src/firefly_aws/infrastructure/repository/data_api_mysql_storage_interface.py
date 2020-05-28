f1a8002f54d5fe263df758689ef7dc0bda8edbf3
jdwil <me@jdwilliams.xyz>
2020-05-27 19:47:57 -0500

    Saving progress

--- src/firefly_aws/infrastructure/repository/data_api_mysql_storage_interface.py
+++ src/firefly_aws/infrastructure/repository/data_api_mysql_storage_interface.py
@@ -14,6 +14,8 @@
 
 from __future__ import annotations
 
+from dataclasses import fields
+from datetime import datetime
 from typing import Type
 
 import firefly as ff
@@ -29,16 +31,54 @@ class DataApiMysqlStorageInterface(ffi.DbApiStorageInterface):
     _db_secret_arn: str = None
     _db_name: str = None
 
+    def __init__(self):
+        super().__init__()
+        self._cache = {
+            'sql': {},
+            'indexes': {},
+        }
+
     def _disconnect(self):
         pass
 
     def _add(self, entity: ffd.Entity):
-        sql = f"insert into {self._fqtn(entity.__class__)} (id, obj) values (:id, :obj)"
+        entity_type = entity.__class__
+        key = entity_type.__name__
+        if key not in self._cache['sql']:
+            cols = [f.name for f in self._get_indexes(entity_type)]
+            placeholders = [f':{f.name}' for f in self._get_indexes(entity_type)]
+
+            if len(cols) > 0:
+                cols = f",{','.join(cols)}"
+                placeholders = f",{','.join(placeholders)}"
+            else:
+                cols = ''
+                placeholders = ''
+            self._cache['sql'][key] = f"""
+                insert into {self._fqtn(entity.__class__)} (id, obj{cols}) 
+                values (:id, :obj{placeholders})
+            """
+
         params = [
             {'name': 'id', 'value': {'stringValue': entity.id_value()}},
             {'name': 'obj', 'value': {'stringValue': self._serializer.serialize(entity)}},
         ]
-        ff.retry(lambda: self._exec(sql, params))
+        for field_ in self._get_indexes(entity_type):
+            t = 'stringValue'
+            val = getattr(entity, field_.name)
+            if field_.type == 'float':
+                t = 'doubleValue'
+            elif field_.type == 'int':
+                t = 'longValue'
+            elif field_.type == 'bool':
+                t = 'booleanValue'
+            elif field_.type == 'bytes':
+                t = 'blobValue'
+            elif field_.type == 'datetime':
+                val = str(val)
+            params.append({'name': field_.name, 'value': {t: val}})
+
+        ff.retry(lambda: self._exec(self._cache['sql'][key], params))
 
     def _all(self, entity_type: Type[ffd.Entity], criteria: ffd.BinaryOp = None, limit: int = None):
         sql = f"select obj from {self._fqtn(entity_type)}"
@@ -83,13 +123,41 @@ class DataApiMysqlStorageInterface(ffi.DbApiStorageInterface):
         return True
 
     def _ensure_table_created(self, entity: Type[ffd.Entity]):
+        columns = []
+        indexes = []
+        for i in self._get_indexes(entity):
+            indexes.append(f'INDEX idx_{i.name} (`{i.name}`)')
+            if i.type == 'float':
+                columns.append(f"`{i.name}` float")
+            elif i.type == 'int':
+                columns.append(f"`{i.name}` integer")
+            elif i.type == 'datetime':
+                columns.append(f"`{i.name}` datetime")
+            else:
+                length = i.metadata['length'] if 'length' in i.metadata else 256
+                columns.append(f"`{i.name}` varchar({length})")
+        extra = ''
+        if len(columns) > 0:
+            extra = f", {','.join(columns)}, {','.join(indexes)}"
+
         sql = f"""
             create table if not exists {self._fqtn(entity)} (
-                id varchar(36) primary key,
-                obj longtext not null
+                id varchar(36)
+                , obj longtext not null
+                {extra}
+                , primary key(id)
             )
         """
         self._exec(sql, [])
+
+    def _get_indexes(self, entity: Type[ffd.Entity]):
+        if entity not in self._cache['indexes']:
+            self._cache['indexes'][entity] = []
+            for field_ in fields(entity):
+                if 'index' in field_.metadata and field_.metadata['index'] is True:
+                    self._cache['indexes'][entity].append(field_)
+
+        return self._cache['indexes'][entity]
 
     @staticmethod
     def _fqtn(entity: Type[ffd.Entity]):
