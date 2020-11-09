8b111825686dd17ea5e738ce2d78df184763a2f4
jdwil <me@jdwilliams.xyz>
2020-11-09 13:26:37 -0600

    New release.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name='firefly-aws',
-    version='1.1.2',
+    version='1.1.3',
     author="",
     author_email="",
     description="AWS extension for the Firefly framework.",
@@ -16,7 +16,7 @@ setuptools.setup(
         'cognitojwt>=1.2.2',
         'dateparser>=0.7.4',
         'firefly-dependency-injection>=1.0.0',
-        'firefly-framework>=1.1.0',
+        'firefly-framework>=1.1.9',
         'Jinja2>=2.11.1',
         'jinjasql>=0.1.8',
         'requests>=2.23.0',
