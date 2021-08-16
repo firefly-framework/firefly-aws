07f3a296f67507de19a558f7ab345fea9e58a34c
jdwil <me@jdwilliams.xyz>
2021-08-16 13:47:48 -0500

    A basic version of adaptive memory. Still some work to do here, but you can at least hard-code memory limits for specific messages.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name='firefly-aws',
-    version='1.1.55',
+    version='1.1.56',
     author="",
     author_email="",
     description="AWS extension for the Firefly framework.",
