11b846c31d4641536ef0c95a34556126cdedcd13
jdwil <me@jdwilliams.xyz>
2021-08-18 08:41:13 -0500

    New release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name='firefly-aws',
-    version='1.1.61',
+    version='1.1.62',
     author="",
     author_email="",
     description="AWS extension for the Firefly framework.",
